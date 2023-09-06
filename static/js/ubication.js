a0bb6841a71c96c4ab62b38898422aeff2418e32
Matias Celiz <maticelizramos@gmail.com>
2023-09-06 00:32:48 -0300

    changes

--- static/js/ubication.js
+++ static/js/ubication.js
@@ -1,5 +1,12 @@
 if ('geolocation' in navigator) {
     navigator.geolocation.getCurrentPosition(function(position) {
+
+        const locButton = document.querySelector('.loc-button');
+        const todayInfo = document.querySelector('.today-info');
+        const todayWeatherIcon = document.querySelector('.today-weather i');
+        const todayTemp = document.querySelector('.weather-temp');
+        const daysList = document.querySelector('.days-list');
+
         var latitude = position.coords.latitude;
         var longitude = position.coords.longitude;
 
@@ -8,6 +15,10 @@ if ('geolocation' in navigator) {
         xhr.onreadystatechange = function() {
             if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
                 var response = JSON.parse(xhr.responseText);
+                
+                todayInfo.querySelector('h2').textContent = response.today
+                todayInfo.querySelector('span').textContent = response.date
+                todayWeatherIcon.className = `bx bx-${response.icon}`;
                 document.getElementById('temperature').textContent = response.temperature + "°C";
                 document.getElementById('humidity').textContent = response.humidity + "%";
                 document.getElementById('description').textContent = response.description;
