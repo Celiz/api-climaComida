6497d4cd56401dd5e53c80b674937e98517aba7d
Matias Celiz <maticelizramos@gmail.com>
2023-09-07 00:01:03 -0300

    changes in api url

--- app.py
+++ app.py
@@ -5,62 +5,78 @@ import requests
 
 app = Flask(__name__)
 
+API_KEY = "31e3dfa472160d5972bfc0a67c451fc0"
+API_URL = "http://api.openweathermap.org/data/2.5/forecast?"
+
+locale.setlocale(locale.LC_TIME, 'es_ES')
+
+def get_weather_data(latitude, longitude):
+    complete_url = f"{API_URL}appid={API_KEY}&lat={latitude}&lon={longitude}&units=metric&lang=es"
+    response = requests.get(complete_url)
+    data = response.json()
+
+    print(complete_url)
+    return data
+
+def format_weather_data(data):
+    if data.get("cod") != 404:
+        main_data = data["list"][0]["main"]
+        weather_data = data["list"][0]["weather"][0]
+        clouds_data = data["list"][0]["clouds"]
+        wind_data = data["list"][0]["wind"]
+
+        today = datetime.datetime.now().strftime('%A')
+        today = today.capitalize()
+        date = datetime.datetime.now().strftime('%d %B %Y')
+        
+        nextDaysData = []
+        for i in range(1, 5):
+            nextDay = datetime.datetime.now() + datetime.timedelta(days=i)
+            nextDay = nextDay.strftime('%A')
+            nextDay = nextDay.capitalize()
+            nextDaysData.append(nextDay)
+
+
+        formatted_data = {
+            "temperature": round(main_data["temp"]),
+            "humidity": main_data["humidity"],
+            "description": weather_data["description"],
+            "feelsLike": round(main_data["feels_like"]),
+            "icon": weather_data["icon"],
+            "clouds": clouds_data["all"],
+            "today": today,
+            "date": date,
+            "wind": round(wind_data["speed"]*2.63),
+            "city": data["city"]["name"],
+            "country": data["city"]["country"],
+            "nextDaysData": nextDaysData
+
+        }
+
+        print(formatted_data)
+        return formatted_data
+    else:
+        return None
+
 @app.route('/')
 def index():
     return render_template('app.html')
 
 @app.route('/weather')
 def weather():
-    apikey = "31e3dfa472160d5972bfc0a67c451fc0"
-    apiUrl = "http://api.openweathermap.org/data/2.5/weather?"
     latitude = request.args.get('lat')
     longitude = request.args.get('lon')
     
-    locale.setlocale(locale.LC_TIME, 'es_ES')
+    weather_data = get_weather_data(latitude, longitude)
 
-    diaSemana = ["Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado", "Domingo"]
-    todayEsp = diaSemana[datetime.datetime.now().weekday()]
-    currentDate = datetime.datetime.now().strftime('%d %B %Y')
-    complete_url = apiUrl + f"appid={apikey}&lat={latitude}&lon={longitude}&units=metric&lang=es"
-    response = requests.get(complete_url)
-    data = response.json()
-    
-    print(complete_url)
-
-    if data["cod"] != "404":
-        y = data["main"]
-        todayTemperature = y["temp"]
-        todayFeelsLike = y["feels_like"]
-        todayHumidity = y["humidity"]
-        todayWind = data["wind"]["speed"]
-        #get city name, and country
-        todayCity = data["name"]
-        todayCountry = data["sys"]["country"]
-        z = data["weather"]
-        todayDescription = z[0]["description"]
-        todayIcon = z[0]["icon"]
-        w = data["clouds"]
-        todayClouds = w["all"]
-
-
-        weather_data = {
-            "temperature": round(todayTemperature),
-            "humidity": todayHumidity,
-            "description": todayDescription,
-            "feels_like": round(todayFeelsLike),
-            "icon": todayIcon,
-            "clouds": todayClouds,
-            "today": todayEsp,
-            "date": currentDate,
-            "wind": todayWind,
-            "city": todayCity,
-            "country": todayCountry
-        }
-        print(weather_data)
-
-        return jsonify(weather_data)
+    if weather_data:
+        formatted_data = format_weather_data(weather_data)
+        if formatted_data:
+            return jsonify(formatted_data)
+        else:
+            return jsonify({"error": "City Not Found"})
     else:
-        return jsonify({"error": "City Not Found"})
-    
+        return jsonify({"error": "Weather data not available"})
+
 if __name__ == '__main__':
     app.run()
--- static/js/ubication.js
+++ static/js/ubication.js
@@ -1,76 +1,80 @@
-if ("geolocation" in navigator) {
-  navigator.geolocation.getCurrentPosition(function (position) {
-    const weatherIconMap = {
-      "01d": "sun",
-      "01n": "moon",
-      "02d": "sun",
-      "02n": "moon",
-      "03d": "cloud",
-      "03n": "cloud",
-      "04d": "cloud",
-      "04n": "cloud",
-      "09d": "cloud-rain",
-      "09n": "cloud-rain",
-      "10d": "cloud-rain",
-      "10n": "cloud-rain",
-      "11d": "cloud-lightning",
-      "11n": "cloud-lightning",
-      "13d": "cloud-snow",
-      "13n": "cloud-snow",
-      "50d": "water",
-      "50n": "water",
-    };
+document.addEventListener("DOMContentLoaded", function () {
+    if ("geolocation" in navigator) {
+      navigator.geolocation.getCurrentPosition(function (position) {
 
-    const locButton = document.querySelector(".loc-button");
-    const todayInfo = document.querySelector(".today-info");
-    const todayWeatherIcon = document.querySelector(".today-weather i");
-    const todayTemp = document.querySelector(".weather-temp");
-    const daysList = document.querySelector(".days-list");
-    const locationElement = document.querySelector('.today-info > div > span');
-
-    var latitude = position.coords.latitude;
-    var longitude = position.coords.longitude;
-
-    var xhr = new XMLHttpRequest();
-    xhr.open("GET", `/weather?lat=${latitude}&lon=${longitude}`, true);
-    xhr.onreadystatechange = function () {
-      if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
-        var response = JSON.parse(xhr.responseText);
-
-        todayInfo.querySelector("h2").textContent = response.today;
-        todayInfo.querySelector("span").textContent = response.date;
-        todayWeatherIcon.className = `bx bx-${weatherIconMap[response.icon]}`;
-        document.getElementById("temperature").textContent = response.temperature + "°C";
-        document.getElementById("description").textContent = response.description;
-        locationElement.textContent = `${response.city}, ${response.country}`;
-       
-
-        // Update todays info in the "day-info" section
-        const todayPrecipitation = `${response.clouds}%`;
-        const todayHumidity = `${response.humidity}%`;
-        const todayWindSpeed = `${response.wind} km/h`;
-
-        const dayInfoContainer = document.querySelector(".day-info");
-        dayInfoContainer.innerHTML = `
-
-            <div>
-                <span class="title">Precipitaciones</span>
-                <span class="value">${todayPrecipitation}</span>
-            </div>
-            <div>
-                <span class="title">Humedad</span>
-                <span class="value">${todayHumidity}</span>
-            </div>
-            <div>
-                <span class="title">Viento</span>
-                <span class="value">${todayWindSpeed}</span>
-            </div>
-
-        `;
-      }
-    };
-    xhr.send();
+        const weatherIconMap = {
+            "01d": "sun",
+            "01n": "moon",
+            "02d": "sun",
+            "02n": "moon",
+            "03d": "cloud",
+            "03n": "cloud",
+            "04d": "cloud",
+            "04n": "cloud",
+            "09d": "cloud-rain",
+            "09n": "cloud-rain",
+            "10d": "cloud-rain",
+            "10n": "cloud-rain",
+            "11d": "cloud-lightning",
+            "11n": "cloud-lightning",
+            "13d": "cloud-snow",
+            "13n": "cloud-snow",
+            "50d": "water",
+            "50n": "water",
+          };
+  
+        const todayInfo = document.querySelector(".today-info");
+        const todayWeatherIcon = document.querySelector(".today-weather i");
+        const todayTemp = document.querySelector(".weather-temp");
+        const daysList = document.querySelector(".days-list");
+        const locationElement = document.querySelector('.today-info > div > span');
+  
+        const latitude = position.coords.latitude;
+        const longitude = position.coords.longitude;
+  
+        const xhr = new XMLHttpRequest();
+        xhr.open("GET", `/weather?lat=${latitude}&lon=${longitude}`, true);
+        xhr.onreadystatechange = function () {
+          if (xhr.readyState === XMLHttpRequest.DONE) {
+            if (xhr.status === 200) {
+              const response = JSON.parse(xhr.responseText);
+  
+              todayInfo.querySelector("h2").textContent = response.today;
+              todayInfo.querySelector("span").textContent = response.date;
+              todayWeatherIcon.className = `bx bx-${weatherIconMap[response.icon]}`;
+              document.getElementById("temperature").textContent = response.temperature + "°C";
+              document.getElementById("description").textContent = response.description;
+              locationElement.textContent = `${response.city}, ${response.country}`;
+  
+              // Actualizar información en la sección "day-info"
+              const todayPrecipitation = `${response.clouds}%`;
+              const todayHumidity = `${response.humidity}%`;
+              const todayWindSpeed = `${response.wind} km/h`;
+  
+              const dayInfoContainer = document.querySelector(".day-info");
+              dayInfoContainer.innerHTML = `
+                <div>
+                  <span class="title">Precipitaciones</span>
+                  <span class="value">${todayPrecipitation}</span>
+                </div>
+                <div>
+                  <span class="title">Humedad</span>
+                  <span class="value">${todayHumidity}</span>
+                </div>
+                <div>
+                  <span class="title">Viento</span>
+                  <span class="value">${todayWindSpeed}</span>
+                </div>
+              `;
+            } else {
+              console.error("Error al obtener datos del clima:", xhr.status);
+            }
+          }
+        };
+        xhr.send();
+      });
+    } else {
+      console.log("La geolocalización no está disponible");
+    }
   });
-} else {
-  console.log("Geolocation is not available");
-}
+  
