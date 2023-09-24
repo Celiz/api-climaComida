c101f0e6f60f1b31ed37264faee9cdf45f69685a
Matias Celiz <maticelizramos@gmail.com>
2023-09-24 01:05:09 -0300

    promises

--- static/js/ubication.js
+++ static/js/ubication.js
@@ -11,7 +11,10 @@ document.addEventListener("DOMContentLoaded", function () {
           const temperatura = weatherData.temperature;
 
           const recetasSugeridas = obtenerRecetasSugeridas(region, temperatura);
-          mostrarComidas(recetasSugeridas); 
+          console.log(recetasSugeridas);
+
+          mostrarRecetasSugeridas(region, temperatura);
+         
         });
       });
     }, function (error) {
@@ -54,76 +57,62 @@ function fetchLocationData(latitude, longitude, callback) {
   xhr.send();
 }
 
-function obtenerRecetasSugeridas(region, temperatura) {
-  const fechaActual = obtenerFechaActual(); 
-  localStorage.removeItem(`recetas_${fechaActual}`);
-  const recetasAlmacenadas = localStorage.getItem(`recetas_${fechaActual}`);
-  if (recetasAlmacenadas) {
-    return JSON.parse(recetasAlmacenadas);
-  } else {
-    const recetasPorRegion = {
-      "Región Norte": [
-        { nombre: "Empanadas Salteñas",ingredientes: ["carne molida", "cebolla", "aceitunas", "huevo"], temperaturaIdeal: 25 },
-        { nombre: "Humita en Chala", ingredientes: ["choclo", "queso", "cebolla", "manteca"], temperaturaIdeal: 20 },
-      ],
-      "Región Centro": [
-        { nombre: "Asado Criollo", ingredientes: ["carne asada", "sal", "pimienta"], temperaturaIdeal: 30 },
-        { nombre: "Milanesa a la Napolitana", pasos: "prueba",ingredientes: ["filete de carne", "pan rallado", "jamón", "queso", "salsa de tomate"], temperaturaIdeal: 25 },
-        { nombre: "Ñoquis", ingredientes: ["papa", "harina", "sal"], temperaturaIdeal: 20 },
-        { nombre: "Locro", pasos: "prueba", ingredientes: ["maíz", "carne", "chorizo", "cebolla", "pimiento", "pimentón"], temperaturaIdeal: 15 },
-        { nombre: "Tarta de Jamón y Queso", ingredientes: ["masa de tarta", "jamón", "queso", "huevos", "crema"], temperaturaIdeal: 10 },
-      ],
-      "Región Noreste": [
-        { nombre: "Chipá", ingredientes: ["almidón de mandioca", "queso", "huevo"], temperaturaIdeal: 30 },
-        { nombre: "Sopa Paraguaya", ingredientes: ["harina de maíz", "cebolla", "queso", "leche"], temperaturaIdeal: 25 },
-      
-      ],
-      "Región Cuyo": [
-        { nombre: "Carbonada", ingredientes: ["carne", "papa", "choclo", "cebolla", "tomate"], temperaturaIdeal: 30 },
-        { nombre: "Pastelitos", ingredientes: ["masa para pastelitos", "dulce de membrillo"], temperaturaIdeal: 25 },
-      
-      ],
-      "Región Patagónica": [
-        { nombre: "Cordero Patagónico", ingredientes: ["cordero", "papa", "cebolla", "zanahoria"], temperaturaIdeal: 30 },
-        { nombre: "Tortas Fritas", ingredientes: ["harina", "levadura", "agua", "sal"], temperaturaIdeal: 25 },
-        
-      ],
-    };
+async function obtenerRecetasSugeridas(region, temperatura) {
+  const fechaActual = obtenerFechaActual();
+  const recetaAlmacenada = localStorage.getItem(`receta_${fechaActual}`);
 
-    const recetasRegion = recetasPorRegion[region] || [];
+  if (recetaAlmacenada) {
+    return JSON.parse(recetaAlmacenada);
+  }
 
-    const recetasSugeridas = recetasRegion.filter((receta) => {
-      return temperatura <= receta.temperaturaIdeal;
-    });
-    if (recetasSugeridas.length >= 2) {
-      const recetasAleatorias = [];
-      while (recetasAleatorias.length < 2) {
-        const indiceAleatorio = Math.floor(Math.random() * recetasSugeridas.length);
-        recetasAleatorias.push(recetasSugeridas[indiceAleatorio]);
-        recetasSugeridas.splice(indiceAleatorio, 1); // Evita duplicados
+  const response = await fetch('../static/js/recetas.json');
+  const recetasPorRegion = await response.json();
+
+  if (!recetasPorRegion[region]) {
+    console.error(`No se encontraron recetas para la región ${region}`);
+    return [];
+  }
+
+  const recetasRegion = recetasPorRegion[region];
+  const recetasSugeridas = recetasRegion.filter((receta) => {
+    return temperatura <= receta.temperaturaIdeal;
+  });
+
+  if (recetasSugeridas.length >= 2) {
+    const recetasAleatorias = [];
+    while (recetasAleatorias.length < 2) {
+      const recetaAleatoria = recetasSugeridas[Math.floor(Math.random() * recetasSugeridas.length)];
+      if (!recetasAleatorias.includes(recetaAleatoria)) {
+        recetasAleatorias.push(recetaAleatoria);
       }
-    
-      localStorage.setItem(`recetas_${fechaActual}`, JSON.stringify(recetasAleatorias));
-      return recetasAleatorias;
-    } else {
-      return recetasSugeridas;
     }
+    localStorage.setItem(`receta_${fechaActual}`, JSON.stringify(recetasAleatorias));
+    return recetasAleatorias;
+  } else {
+    return [];
   }
 }
 
 function obtenerFechaActual() {
   const fecha = new Date();
   const dia = fecha.getDate();
-  const mes = fecha.getMonth() + 1; // Los meses se indexan desde 0
+  const mes = fecha.getMonth() + 1; 
   const año = fecha.getFullYear();
   return `${año}-${mes < 10 ? "0" : ""}${mes}-${dia < 10 ? "0" : ""}${dia}`;
+}
+
+async function mostrarRecetasSugeridas(region, temperatura) {
+  const recetasSugeridas = await obtenerRecetasSugeridas(region, temperatura);
+  mostrarComidas(recetasSugeridas);
 }
 
 function mostrarComidas(recetas) {
   const recetasContainer = document.querySelector(".recetas-container");
   recetasContainer.innerHTML = "";
 
-  for (const receta of recetas) {
+  for (let i = 0; i < 2 && i < recetas.length; i++) {
+    const receta = recetas[i];
+
     const recetaCard = document.createElement("div");
     recetaCard.className = "receta-card";
 
@@ -134,7 +123,7 @@ function mostrarComidas(recetas) {
 
     const preparacionReceta = document.createElement("p");
     preparacionReceta.className = "preparacion-card";
-    preparacionReceta.textContent = "Preparación: " + receta.pasos;
+    preparacionReceta.textContent = "Preparación: " + receta.preparacion;
     recetaCard.appendChild(preparacionReceta);
 
     const ingredientesReceta = document.createElement("ul");
@@ -148,7 +137,7 @@ function mostrarComidas(recetas) {
 
     recetasContainer.appendChild(recetaCard);
   }
-} 
+}
 
 function updateWeatherInfo(data) {
   const weatherIconMap = {
