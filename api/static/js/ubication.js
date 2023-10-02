b642d5e7ba0c64cf5af95db43f79452b12036d40
Celiz <maticelizramos@gmail.com>
2023-10-02 16:46:09 -0300

    tuki time

--- api/static/js/ubication.js
+++ api/static/js/ubication.js
@@ -60,17 +60,38 @@ function obtenerRecetasSugeridas(region, temperatura) {
   if (recetasAlmacenadas) {
     return JSON.parse(recetasAlmacenadas);
   } else {
+
     const recetasPorRegion = {
       "Región Norte": [
         { nombre: "Empanadas Salteñas",ingredientes: ["carne molida", "cebolla", "aceitunas", "huevo"], temperaturaIdeal: 25 },
         { nombre: "Humita en Chala", ingredientes: ["choclo", "queso", "cebolla", "manteca"], temperaturaIdeal: 20 },
       ],
       "Región Centro": [
-        { nombre: "Asado Criollo", ingredientes: ["carne asada", "sal", "pimienta"], temperaturaIdeal: 30 },
-        { nombre: "Milanesa a la Napolitana", pasos: "prueba",ingredientes: ["filete de carne", "pan rallado", "jamón", "queso", "salsa de tomate"], temperaturaIdeal: 25 },
-        { nombre: "Ñoquis", ingredientes: ["papa", "harina", "sal"], temperaturaIdeal: 20 },
-        { nombre: "Locro", pasos: "prueba", ingredientes: ["maíz", "carne", "chorizo", "cebolla", "pimiento", "pimentón"], temperaturaIdeal: 15 },
-        { nombre: "Tarta de Jamón y Queso", ingredientes: ["masa de tarta", "jamón", "queso", "huevos", "crema"], temperaturaIdeal: 10 },
+              { nombre: "Asado Criollo", ingredientes: ["carne asada", "sal", "pimienta"], temperaturaIdeal: 30, pasos: "Paso 1: Preparar la carne asada\nPaso 2: Agregar sal y pimienta\nPaso 3: Cocinar a la parrilla" },
+              { nombre: "Milanesa a la Napolitana", ingredientes: ["filete de carne", "pan rallado", "jamón", "queso", "salsa de tomate"], temperaturaIdeal: 25, pasos: "Paso 1: Preparar el filete de carne\nPaso 2: Empanizar con pan rallado\nPaso 3: Agregar jamón y queso\nPaso 4: Hornear con salsa de tomate" },
+              { nombre: "Ñoquis", ingredientes: ["papa", "harina", "sal"], temperaturaIdeal: 20, pasos: "Paso 1: Preparar la masa con papa, harina y sal\nPaso 2: Formar los ñoquis\nPaso 3: Cocinar en agua hirviendo" },
+              { nombre: "Locro", ingredientes: ["maíz", "carne", "chorizo", "cebolla", "pimiento", "pimentón"], temperaturaIdeal: 15, pasos: "Paso 1: Cocinar maíz y carne\nPaso 2: Agregar chorizo, cebolla y pimiento\nPaso 3: Condimentar con pimentón" },
+              { nombre: "Tarta de Jamón y Queso", ingredientes: ["masa de tarta", "jamón", "queso", "huevos", "crema"], temperaturaIdeal: 10, pasos: "Paso 1: Forrar un molde con la masa de tarta\nPaso 2: Rellenar con jamón y queso\nPaso 3: Mezclar huevos y crema, verter sobre la tarta\nPaso 4: Hornear" },
+              { nombre: "Plato de pastas calientes", ingredientes: ["pasta", "salsa"], temperaturaIdeal: 10, pasos: "Paso 1: Cocinar la pasta\nPaso 2: Preparar la salsa\nPaso 3: Servir caliente" },
+              { nombre: "Polenta", ingredientes: ["harina de maíz", "agua", "sal"], temperaturaIdeal: 10, pasos: "Paso 1: Cocinar la harina de maíz con agua y sal\nPaso 2: Enfriar y cortar en porciones\nPaso 3: Calentar antes de servir" },
+              { nombre: "Sopa o Guiso", ingredientes: ["caldo", "verduras", "carne"], temperaturaIdeal: 10, pasos: "Paso 1: Preparar el caldo\nPaso 2: Agregar verduras y carne\nPaso 3: Cocinar hasta que esté listo" },
+              { nombre: "Puchero", ingredientes: ["carne", "verduras", "legumbres"], temperaturaIdeal: 10, pasos: "Paso 1: Cocinar carne, verduras y legumbres juntas\nPaso 2: Servir caliente" },
+              { nombre: "Salchichas con arroz", ingredientes: ["salchichas", "arroz"], temperaturaIdeal: 15, pasos: "Paso 1: Cocinar las salchichas\nPaso 2: Preparar el arroz\nPaso 3: Servir juntos" },
+              { nombre: "Milanesas o Hamburguesas", ingredientes: ["carne", "pan", "condimentos"], temperaturaIdeal: 15, pasos: "Paso 1: Preparar las milanesas o hamburguesas\nPaso 2: Cocinar en una sartén o parrilla\nPaso 3: Servir en pan con condimentos" },
+              { nombre: "Pizza", ingredientes: ["masa", "salsa de tomate", "queso", "toppings"], temperaturaIdeal: 15, pasos: "Paso 1: Estirar la masa\nPaso 2: Agregar salsa, queso y toppings\nPaso 3: Hornear hasta que esté dorada" },
+              { nombre: "Tarta de JyQ", ingredientes: ["masa de tarta", "jamón", "queso", "huevos", "crema"], temperaturaIdeal: 15, pasos: "Paso 1: Forrar un molde con la masa de tarta\nPaso 2: Rellenar con jamón y queso\nPaso 3: Mezclar huevos y crema, verter sobre la tarta\nPaso 4: Hornear" },
+              { nombre: "Churrasco con ensalada", ingredientes: ["churrasco", "ensalada"], temperaturaIdeal: 20, pasos: "Paso 1: Cocinar el churrasco\nPaso 2: Preparar la ensalada\nPaso 3: Servir juntos" },
+              { nombre: "Omelette", ingredientes: ["huevos", "relleno"], temperaturaIdeal: 20, pasos: "Paso 1: Batir los huevos\nPaso 2: Cocinar en una sartén con el relleno\nPaso 3: Doble la omelette y sirva" },
+              { nombre: "Empanadas", ingredientes: ["masa", "relleno"], temperaturaIdeal: 20, pasos: "Paso 1: Rellenar la masa con el relleno\nPaso 2: Cerrar y sellar las empanadas\nPaso 3: Hornear hasta que estén doradas" },
+              { nombre: "Pollo frito", ingredientes: ["pollo", "harina", "condimentos"], temperaturaIdeal: 20, pasos: "Paso 1: Rebozar el pollo en harina y condimentos\nPaso 2: Freír hasta que esté dorado y cocido" },
+              { nombre: "Ensalada César", ingredientes: ["lechuga", "pollo", "croutons", "salsa"], temperaturaIdeal: 25, pasos: "Paso 1: Preparar la lechuga, pollo y croutons\nPaso 2: Mezclar con salsa y servir" },
+              { nombre: "Tacos", ingredientes: ["tortillas", "carne", "salsas", "verduras"], temperaturaIdeal: 25, pasos: "Paso 1: Preparar la carne y las tortillas\nPaso 2: Agregar salsas y verduras\nPaso 3: Armar los tacos y servir" },
+              { nombre: "Papas Fritas", ingredientes: ["papas", "aceite", "sal"], temperaturaIdeal: 25, pasos: "Paso 1: Cortar las papas en tiras\nPaso 2: Freír en aceite caliente\nPaso 3: Espolvorear con sal y servir" },
+              { nombre: "Tomates rellenos", ingredientes: ["tomates", "relleno"], temperaturaIdeal: 25, pasos: "Paso 1: Vaciar los tomates y rellenar\nPaso 2: Hornear hasta que estén cocidos" },
+              { nombre: "Sanguches de fiambre", ingredientes: ["fiambre", "pan", "condimentos"], temperaturaIdeal: 30, pasos: "Paso 1: Armar el sánguche con fiambre, pan y condimentos\nPaso 2: Servir" },
+              { nombre: "Sushi", ingredientes: ["arroz", "pescado", "alga", "salsa de soja"], temperaturaIdeal: 30, pasos: "Paso 1: Preparar el arroz y los ingredientes\nPaso 2: Armar los rollos de sushi\nPaso 3: Servir con salsa de soja" },
+              { nombre: "Asado", ingredientes: ["carne asada", "sal", "pimienta"], temperaturaIdeal: 30, pasos: "Paso 1: Preparar la carne asada\nPaso 2: Agregar sal y pimienta\nPaso 3: Cocinar a la parrilla" },
+              { nombre: "Picada", ingredientes: ["fiambres", "quesos", "pan", "salsas"], temperaturaIdeal: 30, pasos: "Paso 1: Preparar una selección de fiambres y quesos\nPaso 2: Servir con pan y salsas" }
       ],
       "Región Noreste": [
         { nombre: "Chipá", ingredientes: ["almidón de mandioca", "queso", "huevo"], temperaturaIdeal: 30 },
@@ -136,8 +157,7 @@ function mostrarComidas(recetas) {
     recetaCard.appendChild(tituloReceta);
     const preparacionReceta = document.createElement("p");
     preparacionReceta.className = "preparacion-card";
-    preparacionReceta.textContent = "Preparación: " + receta.preparacion;
-    recetaCard.appendChild(preparacionReceta);
+    preparacionReceta.textContent = "Preparación: " + receta.pasos; 
     const ingredientesReceta = document.createElement("ul");
     ingredientesReceta.className = "ingredientes-card";
     receta.ingredientes.forEach((ingrediente) => {
@@ -145,8 +165,9 @@ function mostrarComidas(recetas) {
       li.textContent = ingrediente;
       ingredientesReceta.appendChild(li);
     });
-    recetaCard.appendChild(ingredientesReceta);
     recetasContainer.appendChild(recetaCard);
+    recetaCard.appendChild(ingredientesReceta);
+    recetaCard.appendChild(preparacionReceta);
   }
 }
 
@@ -172,7 +193,6 @@ function updateWeatherInfo(data) {
     "50n": "water",
   };
 
-
   const todayInfo = document.querySelector(".today-info");
   const todayWeatherIcon = document.querySelector(".today-weather i");
   const daysList = document.querySelector(".days-list");
@@ -185,7 +205,6 @@ function updateWeatherInfo(data) {
   document.getElementById("description").textContent = data.description;
   locationElement.textContent = `${data.city}, ${data.country}`;
 
-  
   const todayPrecipitation =  (`${data.pop}%`);
   const todayHumidity = `${data.humidity}%`;
   const todayWindSpeed = `${data.wind} km/h`;
